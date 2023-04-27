ca82b256a06b9e1d9cb1a234d1fb114ba9e60ce7
Santimendezl <santimlacosta@gmail.com>
2023-04-27 07:40:43 +0000

    First commit

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -2,6 +2,12 @@ const getState = ({ getStore, setStore }) => {
 	return {
 		store: {
 			//Your data structures, A.K.A Entities
+			contactList: {
+				name: "Santi Mendez",
+				email: "santi@gmail.com",
+				phone: "633100200",
+				address: "Cardedeu"
+			}
 		},
 		actions: {
 			//(Arrow) Functions that update the Store
--- src/js/views/AddContact.js
+++ src/js/views/AddContact.js
@@ -1,36 +1,70 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, {
+    useContext
+} from "react";
+import {
+    Link
+} from "react-router-dom";
+import {
+    Context
+} from "../store/appContext.js";
 
 export const AddContact = () => {
-	return (
-		<div className="container">
-			<div>
-				<h1 className="text-center mt-5">Add a new contact</h1>
-				<form>
-					<div className="form-group">
-						<label>Full Name</label>
-						<input type="text" className="form-control" placeholder="Full Name" />
-					</div>
-					<div className="form-group">
-						<label>Email</label>
-						<input type="email" className="form-control" placeholder="Enter email" />
-					</div>
-					<div className="form-group">
-						<label>Phone</label>
-						<input type="phone" className="form-control" placeholder="Enter phone" />
-					</div>
-					<div className="form-group">
-						<label>Address</label>
-						<input type="text" className="form-control" placeholder="Enter address" />
-					</div>
-					<button type="button" className="btn btn-primary form-control">
-						save
-					</button>
-					<Link className="mt-3 w-100 text-center" to="/">
-						or get back to contacts
-					</Link>
-				</form>
-			</div>
-		</div>
-	);
+    //Inicializamos useContext
+    const {
+        store,
+        actions
+    } = useContext(Context);
+
+    return ( <
+        div className = "container" >
+        <
+        div >
+        <
+        h1 className = "text-center mt-5" > Add a new contact < /h1> <
+        form >
+        <
+        div className = "form-group" >
+        <
+        label > Full Name < /label> <
+        input type = "text"
+        className = "form-control"
+        placeholder = "Full Name" / >
+        <
+        /div> <
+        div className = "form-group" >
+        <
+        label > Email < /label> <
+        input type = "email"
+        className = "form-control"
+        placeholder = "Enter email" / >
+        <
+        /div> <
+        div className = "form-group" >
+        <
+        label > Phone < /label> <
+        input type = "phone"
+        className = "form-control"
+        placeholder = "Enter phone" / >
+        <
+        /div> <
+        div className = "form-group" >
+        <
+        label > Address < /label> <
+        input type = "text"
+        className = "form-control"
+        placeholder = "Enter address" / >
+        <
+        /div> <
+        button type = "button"
+        className = "btn btn-primary form-control" >
+        save <
+        /button> <
+        Link className = "mt-3 w-100 text-center"
+        to = "/" >
+        or get back to contacts <
+        /Link> < /
+        form > <
+        /div> < /
+        div >
+    );
 };
